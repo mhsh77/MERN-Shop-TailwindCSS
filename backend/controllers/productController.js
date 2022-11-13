088400af8b09301d6f0889bc624a61bc9f657eba
mhsh77 <shahsavarimohammad@gmail.com>
2022-11-13 02:58:36 -0800

    end

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -16,3 +16,17 @@ exports.getProducts = async(req,res,next)=>{
         products
     })
 }
+
+exports.getSingleProduct = async(req,res,next) => {
+    const product = await Product.findById(req.params.id)
+    if(!product){
+        return res.status(404).json({
+            success:true,
+            message:'Product not found'
+        })
+    }
+    res.status(200).json({
+        success:true,
+        product
+    })
+}
