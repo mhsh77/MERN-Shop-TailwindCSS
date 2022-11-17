0934ba69ddfe2d36a3f5ce304d7eb84d8fcf45a0
mhsh77 <shahsavarimohammad@gmail.com>
2022-11-17 07:11:52 -0800

    endsearch

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express();
+
 const errorHandler = require('./middlewares/errors');
 
 app.use(express.json());
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,11 @@
 const Product = require('../models/product')
+
 const ErrorHandler = require('../utilites/errorHandler')
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
+const APIFeatures = require('../utilites/apifeatures')
+
 exports.newProduct =catchAsyncErrors( async (req,res,next) => {
+    const apifeatures = new apifeatures(Product.find(),req.query)
     const product = await Product.create(req.body)
     res.status(201).json({
         success:true,
@@ -10,7 +14,10 @@ exports.newProduct =catchAsyncErrors( async (req,res,next) => {
 })
 
 exports.getProducts =catchAsyncErrors( async(req,res,next)=>{
-    const products = await Product.find()
+    const apifeatures = new APIFeatures(Product.find(),req.query)
+        .search()
+
+    const products = await apifeatures.query;
     res.status(200).json({
         success:true,
         count:products.length,
