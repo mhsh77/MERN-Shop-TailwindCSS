49059253db674748fc89d30224ae5d6ecfa67a10
mhsh77 <shahsavarimohammad@gmail.com>
2022-12-02 23:22:16 -0800

    end pre

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { newOrder ,myOrders, getSingleOrder, allOrders} = require('../controllers/orderController')
+const { newOrder ,myOrders, getSingleOrder, allOrders, updateOrder} = require('../controllers/orderController')
 
 const {isAuthenticatedUser, authorizedRoles} = require('../middlewares/auth')
 
@@ -10,4 +10,6 @@ router.route('/order/new').post(isAuthenticatedUser,newOrder)
 router.route('/order/:id').get(isAuthenticatedUser,getSingleOrder)
 router.route('/orders/me').get(isAuthenticatedUser, myOrders)
 router.route('/admin/orders').get(isAuthenticatedUser,authorizedRoles('admin'),allOrders)
+router.route('/admin/order/:id').put(isAuthenticatedUser,authorizedRoles('admin'),updateOrder)
+
 module.exports = router;
